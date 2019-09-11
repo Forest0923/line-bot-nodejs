da0967bfdfd8774ad5b8cdda221bc15f9d75614f
Glitch (forest0923-line-bot-nodejs) <none>
2019-09-11 02:52:44 +0000

    🛍🎟 Updated with Glitch

--- index.js
+++ index.js
@@ -11,38 +11,44 @@ const bot = new linebot({
   verify: true
 });
 
+let USERS = [];
+
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
 
 bot.on('message', function (event) {
-  event.reply([
-    {  
-      "type": "flex",
-      "altText": "this is a flex message",
-      "contents": {
-        "type": "bubble",
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "text",
-              "text": "Hello"
-            },
-            {
-              "type": "text",
-              "text": "world!"
-            }
-          ]
-        }
-      }
-    }
-  ]).then(function (data) {
-    console.log('Success', data);
+  console.log(event);
+  console.log(USERS);
+  event.reply(
+    event.message.text
+  ).then(function (data) {
+    console.log('Success');
   }).catch(function (error) {
-    console.log('Error', error);
+    console.log('Error');
   });
 });
+bot.on('follow', function(event){
+  console.log('Followed by ' + event.source.userId);
+  USERS.push(event.source.userId);
+  event.reply(
+    'Thank you!!'
+  ).then(function(data){
+    console.log('Success: follow')
+  }).catch(function(error){
+    console.log('Error: follow')
+  })
+})
+bot.on('unfollow', function(event){
+  console.log('Unfollowed by ' + event.source.userId);
+  USERS = USERS.filter(n => n !== event.source.userId);
+  event.reply(
+    'byebye:)'
+  ).then(function(data){
+    console.log('Success: unfollow')
+  }).catch(function(error){
+    console.log('Error: unfollow')
+  })
+})
 app.listen(process.env.PORT || 80, function () {
   console.log('LineBot is running.');
 });
