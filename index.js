d65d0f4da40d5190d0758f93a127c3fcbaa3fb33
Glitch (forest0923-line-bot-nodejs) <none>
2019-09-11 05:16:10 +0000

    🚒🏨 Updated with Glitch

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-const express = require('express');
+;const express = require('express');
 const linebot = require('linebot');
 require('dotenv').config();
 
 const app = express();
 
-const bot = new linebot({
-  channelId: process.env.CHANNEL_ID,
-  channelSecret: process.env.CHANNEL_SECRET,
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+const bot = new linebot({   channelId: process.env.CHANNEL_ID,   channelSecret: process.env.CHANNEL_SECRET,   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   verify: true
 });
-
-let USERS = [];
 
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
 
 bot.on('message', function (event) {
   console.log(event);
-  console.log(USERS);
   event.reply(
     event.message.text
   ).then(function (data) {
     console.log('Success');
   }).catch(function (error) {
     console.log('Error');
-  });
-});
+  })
+  bot.broadcast("hi\uDBC0\uDC84"); // emoji;
+})
+;
 bot.on('follow', function(event){
-  console.log('Followed by ' + event.source.userId);
-  USERS.push(event.source.userId);
+  console.log('Followed by ' + event.source.userId)
+  // console.log(event);;
   event.reply(
     'Thank you!!'
   ).then(function(data){
@@ -37,18 +33,13 @@ bot.on('follow', function(event){
   }).catch(function(error){
     console.log('Error: follow')
   })
-})
+}
+)
 bot.on('unfollow', function(event){
-  console.log('Unfollowed by ' + event.source.userId);
-  USERS = USERS.filter(n => n !== event.source.userId);
-  event.reply(
-    'byebye:)'
-  ).then(function(data){
-    console.log('Success: unfollow')
-  }).catch(function(error){
-    console.log('Error: unfollow')
-  })
-})
+  console.log('Unfollowed by ' + event.source.userId)
+  // console.log(event);)
+}
+)
 app.listen(process.env.PORT || 80, function () {
   console.log('LineBot is running.');
-});
+})
